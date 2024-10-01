27cd259c4d829585c0d330062d60fd6f5851b89c
Rimmert Zelle <rimmert.zelle@gmail.com>
2024-10-01 16:22:23 +0200

    added cors

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { getAllPets, updatePet, getPet } from '../controllers/petsController.js';
 import { checkName } from '../middleware/exampleMiddleware.js';
+import cors from 'cors';
 const router = express.Router();
 
 // routes
-router.get('/', (req, res, next) => {
+router.get('/', cors(), (req, res, next) => {
   res.json('hi');
 });
-router.get('/pets', checkName, getAllPets);
-router.get('/pets/:id', checkName, getPet);
+router.get('/pets', cors(), checkName, getAllPets);
+router.get('/pets/:id', cors(), checkName, getPet);
 
 export default router;
